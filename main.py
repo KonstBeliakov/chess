7c11b16e3968795bcdda2776173298398a76873e
KonstBeliakov <konstbeliakov@gmail.com>
2024-07-02 11:17:54 +0300

    settings.py file added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from chess_pieces_moves import *
 from copy import deepcopy
+from settings import *
 import pygame
 
 
@@ -46,19 +47,13 @@ pygame.display.set_caption("Chess")
 
 images = {name: pygame.image.load(f"textures/{name}") for name in os.listdir('textures')}
 
-square_size = 100
-
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((0, 0, 0))
-
-    black_square_color = (50, 50, 50)
-    white_square_color = (200, 200, 200)
-    move_hint_color = (100, 255, 100)
+    screen.fill(bg_color)
 
     # display board
     for x in range(8):
@@ -68,18 +63,20 @@ while running:
             else:
                 color = white_square_color
 
-            pygame.draw.rect(screen, color, (50 + x * square_size, 50 + y * square_size, square_size, square_size))
+            pygame.draw.rect(screen, color, (indentX + x * square_size, indentY + y * square_size, square_size, square_size))
 
     # display pieces
     for x in range(8):
         for y in range(8):
             if board[x][y]:
-                screen.blit(images[f'{board[x][y]}.png'], (50 + y * square_size, 50 + x * square_size))
+                screen.blit(images[f'{board[x][y]}.png'], (indentX + y * square_size, indentY + x * square_size))
 
     # display move hints
-    for x, y in moves:
-        pygame.draw.circle(screen, move_hint_color,
-                           (50 + y * square_size + square_size // 2, 50 + x * square_size + square_size // 2), 10)
+    if moves is not None and show_move_hints:
+        for x, y in moves:
+            pygame.draw.circle(screen, move_hint_color,
+                               (indentX + y * square_size + square_size // 2,
+                                indentY + x * square_size + square_size // 2), 10)
 
 
     pygame.display.flip()
---
+++ settings.py
@@ -0,0 +1,10 @@
+square_size = 100
+indentX = 50
+indentY = 50
+
+bg_color = (0, 0, 0)
+black_square_color = (50, 50, 50)
+white_square_color = (200, 200, 200)
+move_hint_color = (100, 255, 100)
+
+show_move_hints = False
